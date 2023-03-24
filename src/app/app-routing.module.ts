c081e89b86c8bf2fb930985c2cd555ade18449f8
Karimof <islamkarimov8580@gmail.com>
2023-03-24 10:37:19 +0500

    Adding component Hone and Html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {HtmlComponent} from "./html/html.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "html", component: HtmlComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
